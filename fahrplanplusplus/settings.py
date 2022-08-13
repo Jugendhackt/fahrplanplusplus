a2e9d2f19d73723823e65c8c483462365e07e063
Marvin Gaube <dev@marvingaube.de>
2022-08-13 20:54:08 +0200

    Create App

--- fahrplanplusplus/settings.py
+++ fahrplanplusplus/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'production_control.apps.ProductionControlConfig',
+    'dashboard.apps.DashboardConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
--- fahrplanplusplus/urls.py
+++ fahrplanplusplus/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('productioncontrol/', include('production_control.urls')),
     path('admin/', admin.site.urls),
+    path('dashboard/', include('dashboard.urls')),
 ]
 
