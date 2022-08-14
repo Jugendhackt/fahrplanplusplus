b49ff17057a587e53945df36c0a5081f0724d052
Marvin Gaube <dev@marvingaube.de>
2022-08-14 16:26:29 +0200

    Some flake changes

--- dashboard/consumers.py
+++ dashboard/consumers.py
@@ -1,7 +1,3 @@
-# chat/consumers.py
-import json
-
-import channels.layers
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
@@ -9,19 +5,18 @@ import production_control.broadcast
 
 
 class DashboardConsumer(WebsocketConsumer):
-	def connect(self):
-		async_to_sync(self.channel_layer.group_add)("dashboard",self.channel_name)
-		self.accept()
-		production_control.broadcast.broadcast_status()
+    def connect(self):
+        async_to_sync(
+            self.channel_layer.group_add)("dashboard", self.channel_name)
+        self.accept()
+        production_control.broadcast.broadcast_status()
 
-	def disconnect(self, close_code):
-		async_to_sync(self.channel_layer.group_discard)(
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
             "dashboard",
             self.channel_name
         )
-	    # Receive message from room group
-	def dashboard_message(self, event):
-		message = event['data']
 
-		# Send message to WebSocket
-		self.send(text_data=message)
+    def dashboard_message(self, event):
+        message = event['data']
+        self.send(text_data=message)
